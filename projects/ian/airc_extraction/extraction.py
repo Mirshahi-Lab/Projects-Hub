dbb4d5886b5a4d44446a25efc54908d0ece3281e
Ian Dinsmore <iandinsmore1@gmail.com>
2024-04-10 10:05:00 -0400

    Update extraction.py

--- projects/ian/airc_extraction/extraction.py
+++ projects/ian/airc_extraction/extraction.py
@@ -1,30 +1,31 @@
+from datetime import datetime
 from random import randint
 from pydicom import DataElement, Dataset
 
 
 def extract_pulmonary(dicom_data: DataElement) -> dict:
-    pulmonary = dicom_data[0x0040, 0xa730][4][0x0040,0xa730]
+    pulmonary = dicom_data[0x0040, 0xa730][-1][0x0040,0xa730]
     pulmonary_seq_len = len(pulmonary.value)
     pulmonary_dict = {}
     for i in range(pulmonary_seq_len):
-        region = pulmonary[i][0x0040, 0xa730]
-        region_name = region[0][0x0040, 0xa160].value
+        region_name = pulmonary[i][0x0040, 0xa043][0].CodeMeaning.lower().replace(' ', '_')
+        region_measurements = pulmonary[i][0x0040, 0xa730]
+        num_measurements = len(region_measurements.value)
         region_dict = {}
-        max_length = len(region.value)
-        for j in range(7, max_length):
-            label = region[j][0x0040, 0xa043][0][0x0008, 0x0104].value
+        for j in range(num_measurements):
+            label = region_measurements[j][0x0040, 0xa043][0].CodeMeaning
             label = label.lower().replace(' ', '_')
             if label == 'range':
                 value = region[j][0x0040, 0xa168][0].CodeMeaning.lower()
                 region_dict[label] = {'value': value}
             else:
                 try:
-                    label_unit = region[j][0x0040, 0xa300][0][0x0040, 0x08ea][0].CodeValue
-                    value = float(region[j][0x0040, 0xa300][0][0x0040, 0xa30a].value)
+                    label_unit = region_measurements[j][0x0040, 0xa300][0][0x0040, 0x08ea][0].CodeValue
+                    value = float(region_measurements[j][0x0040, 0xa300][0][0x0040, 0xa30a].value)
+
                 except KeyError:
                     label_unit = 'NA'
                     value = 'NA'
-                # Label cleanup
                 if label_unit == '{Number}':
                     label_unit = 'unitless'
                 elif label_unit == "[hnsf'U]":
@@ -37,93 +38,117 @@ def extract_pulmonary(dicom_data: DataElement) -> dict:
 
 
 def extract_parenchyma(dicom_data: DataElement) -> dict:
-    parenchyma = dicom_data[0x0040, 0xa730][4][0x0040, 0xa730]
+    parenchyma = dicom_data[0x0040, 0xa730][-1][0x0040, 0xa730]
     parenchyma_seq_len = len(parenchyma.value)
     parenchyma_dict = {}
     for i in range(parenchyma_seq_len):
-        measurements = parenchyma[i][0x0040, 0xa730]
-        region = measurements[0].TextValue
-        unit = measurements[7][0x0040, 0xa300][0][0x0040, 0x08ea][0].CodeValue
-        value = float(measurements[7][0x0040, 0xa300][0][0x0040, 0xa30a].value)
+        measurements = parenchyma[i]
+        region = measurements[0x0040, 0xa043][0].CodeMeaning.lower().replace(' ', '_')
+        unit = measurements[0x0040, 0xa730][0][0x0040, 0xa300][0][0x0040, 0x08ea][0].CodeValue
+        value = float(measurements[0x0040, 0xa730][0][0x0040, 0xa300][0][0x0040, 0xa30a].value)
         parenchyma_dict[region] = {'unit': unit, 'value': value}
     return parenchyma_dict
 
 
 def extract_cardio(dicom_data: DataElement) -> dict:
-    cardio = dicom_data[0x0040,0xa730][4][0x0040, 0xa730]  # This should always be of length 2
-    heart = cardio[0]
-    cac = cardio[1]
+    cardio = dicom_data[0x0040,0xa730][-1][0x0040, 0xa730]  # This should always be of length 2
+    cardio_len = len(cardio.value)
     cardio_dict = {}
-    for container in [heart, cac]:
-        name = container[0x0040, 0xa730][-1][0x0040, 0xa043][0][0x0008, 0x0104].value.lower().replace(' ', '_')
-        value = container[0x0040, 0xa730][-1][0x0040, 0xa300][0][0x0040, 0xa30a].value
-        if container == heart:
+    # for container in [heart, cac]:
+    #     name = container[0x0040, 0xa730][-1][0x0040, 0xa043][0][0x0008, 0x0104].value.lower().replace(' ', '_')
+    #     value = container[0x0040, 0xa730][-1][0x0040, 0xa300][0][0x0040, 0xa30a].value
+    #     if container == heart:
+    #         cardio_dict[name] = {'unit': 'mL', 'value': value}
+    #     elif container == cac:
+    #         cardio_dict[name] = {'unit': 'mm3', 'value': value}
+    for idx in range(cardio_len):
+        name = cardio[idx][0x0040, 0xa043][0].CodeMeaning.lower().replace(' ', '_')
+        value = cardio[idx][0x0040, 0xa730][-1][0x0040, 0xa300][0][0x0040, 0xa30a].value
+        if name == 'heart':
             cardio_dict[name] = {'unit': 'mL', 'value': value}
-        elif container == cac:
+        elif name == 'total_coronary_calcium':
             cardio_dict[name] = {'unit': 'mm3', 'value': value}
     return cardio_dict
 
 
 def extract_aorta(dicom_data: DataElement) -> dict:
-    aorta = dicom_data[0x0040, 0xa730][4][0x0040, 0xa730]  # This will always have 9 elements
+    aorta = dicom_data[0x0040, 0xa730][-1][0x0040, 0xa730]  # This will always have 9 elements
     aorta_dict = {}
     for i in range(9):
-        aortic_location = aorta[i][0x0040, 0xa730][-4][0x0040, 0xa168][0][0x0008, 0x0104].value
-        aortic_measurement = float(aorta[i][0x0040,0xa730][-2].MeasuredValueSequence[0][0x0040,0xa30a].value)
-        aorta_dict[aortic_location] = {'unit': 'mm', 'value': aortic_measurement}
+        location = aorta[i][0x0040, 0xa043][0].CodeMeaning.lower().replace(' ', '_')
+        diameter = float(aorta[i][0x0040, 0xa730][0][0x0040, 0xa300][0][0x0040, 0xa30a].value)
+        aorta_dict[location] = {'unit': 'mm', 'value': diameter}
     return aorta_dict
 
 
 def extract_spine(dicom_data: DataElement) -> dict:
-    spine = dicom_data[0x0040, 0xa730][4][0x0040, 0xa730]
+    spine = dicom_data[0x0040, 0xa730][-1][0x0040, 0xa730]
     spine_len = range(len(spine.value))
     height_index_dict = {
-        5: 'anterior',
-        6: 'medial',
-        7: 'posterior'
+        0: 'anterior',
+        1: 'medial',
+        2: 'posterior'
     }
     spine_dict = {}
+    spine_name_dict = {
+        'First thoracic vertebra': 'T1',
+        'Second thoracic vertebra': 'T2',
+        'Third thoracic vertebra': 'T3',
+        'Fourth thoracic vertebra': 'T4',
+        'Fifth thoracic vertebra': 'T5',
+        'Sixth thoracic vertebra': 'T6',
+        'Seventh thoracic vertebra': 'T7',
+        'Eighth thoracic vertebra': 'T8',
+        'Ninth thoracic vertebra': 'T9',
+        'Tenth thoracic vertebra': 'T10',
+        'Eleventh thoracic vertebra': 'T11',
+        'Twelfth thoracic vertebra': 'T12'
+    }
     for i in spine_len:
-        vert = spine[i][0x0040, 0xa730]
-        vert_name = vert[0].TextValue
         vert_dict = {}
-        if vert_name != 'Spine Applied Range':
+        name = spine[i][0x0040, 0xa043][0].CodeMeaning
+        if name != 'Spine Applied Range':
+            vert = spine[i][0x0040, 0xa730]
+            vert_name = spine_name_dict.get(name)
             for idx, label in height_index_dict.items():
-                height = vert[idx][0x0040, 0xa300][0][0x0040, 0xa30a].value
+                height = vert[idx][0x0040, 0xa730][0][0x0040, 0xa300][0][0x0040, 0xa30a].value
                 med_range = vert[idx][0x0040, 0xa730][1][0x0040, 0xa168][0].CodeMeaning.lower()
                 vert_dict[f'{label}_height'] = {'unit': 'mm', 'value': height, 'range': med_range}
-            avg_hu = vert[8][0x0040, 0xa300][0][0x0040, 0xa30a].value
+            avg_hu = vert[-1][0x0040, 0xa300][0][0x0040, 0xa30a].value
             vert_dict['attenuation'] = {'unit': 'HU', 'value': avg_hu}
             spine_dict[vert_name] = vert_dict
         else:
-            spine_range = vert[4][0x0040, 0xa168][0].CodeMeaning.lower()
+            vert = spine[i]
+            spine_range = vert[0x0040, 0xa168][0].CodeMeaning.lower()
             spine_dict['spine_med_range'] = spine_range
     return spine_dict
 
+
 def extract_lesions(dicom_data: DataElement) -> dict:
-    lesions = dicom_data[0x0040, 0xa730][4][0x0040, 0xa730]
+    lesions = dicom_data[0x0040, 0xa730][-1][0x0040, 0xa730]
     num_lesions = len(lesions.value)
     lesions_dict = {}
     for i in range(num_lesions):
-        lesion_data = lesions[i][0x0040, 0xa730]
-        lesion_name = lesion_data[0].TextValue
-        lesion_location = lesion_data[3][0x0040, 0xa730][0][0x0040, 0xa168][0].CodeMeaning.lower()
-        long_axis_mm = float(lesion_data[6][0x0040, 0xa300][0][0x0040, 0xa30a].value)
-        mesh_3d_diam = float(lesion_data[7][0x0040, 0xa300][0][0x0040, 0xa30a].value)
-        short_axis_mm = float(lesion_data[8][0x0040, 0xa300][0][0x0040, 0xa30a].value)
-        avg_2d_diameter = float(lesion_data[9][0x0040, 0xa300][0][0x0040, 0xa30a].value)
-        lesion_volume = float(lesion_data[10][0x0040, 0xa300][0][0x0040, 0xa30a].value)
-        lesions_dict[lesion_name] = {
-            'location': lesion_location,
-            'diameters': {
-                'unit': 'mm',
-                'long_axis': long_axis_mm,
-                'short_axis': short_axis_mm,
-                'avg_2d': avg_2d_diameter,
-                '3d_mesh': mesh_3d_diam
-            },
-            'volume': lesion_volume
-        }
+        if lesions[i][0x0040, 0xa043][0].CodeMeaning != 'Tumor Burden':
+            lesion_data = lesions[i][0x0040, 0xa730]
+            lesion_name = lesion_data[0].TextValue
+            lesion_location = lesion_data[4][0x0040, 0xa168][0].CodeMeaning.lower()
+            recist_mm = float(lesion_data[1][0x0040, 0xa300][0][0x0040, 0xa30a].value)
+            mesh_3d_diam = float(lesion_data[2][0x0040, 0xa300][0][0x0040, 0xa30a].value)
+            lesion_volume = float(lesion_data[3][0x0040, 0xa300][0][0x0040, 0xa30a].value)
+            review_status = lesion_data[5][0x0040, 0xa160].value.lower()
+            if 'unreviewed' in review_status:
+                review_status = 'unreviewed'
+            lesions_dict[lesion_name] = {
+                'location': lesion_location,
+                'diameters': {
+                    'unit': 'mm',
+                    'recist': recist_mm,
+                    '3d_mesh': mesh_3d_diam
+                },
+                'volume': {'unit': 'mm3', 'value': lesion_volume},
+                'review_status': review_status
+            }
     return lesions_dict
 
 
@@ -149,7 +174,7 @@ def extract_identifiers(dicom: Dataset) -> dict:
             'default': 'NoProtocolFound',
             'tag_name': 'protocol_name'
         },
-        'AcquisitionDate': {
+        'StudyDate': {
             'default': 'YYYY-MM-DD',
             'tag_name': 'scan_date'
         },
@@ -159,5 +184,8 @@ def extract_identifiers(dicom: Dataset) -> dict:
         output = getattr(dicom, tag, defaults['default'])
         if output == '':
             output = defaults['default']
+        elif tag == 'StudyDate':
+            output = datetime.strptime(output, '%Y%m%d').date()
+            output = output.strftime('%Y-%m-%d')
         identfiers[defaults['tag_name']] = output
     return identfiers
